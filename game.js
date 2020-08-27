42ed78a7f338e196020a32948515e4f8f6ce555c
Gabriel Fernando <gabriel.fernando@rastergr.com.br>
2020-08-26 21:57:35 -0300

    criada funcao para mover cacador

--- game.js
+++ game.js
@@ -8,7 +8,6 @@ $(document).ready(() => {
           const casa = $('<td/>').addClass([`casa-${x}-${y} casa`]);
           const conteudo = $('<div/>').addClass('conteudo').appendTo(casa);
           casa.appendTo(linha);
-          // if (x == y) conteudo.addClass(['personagem cacador']);
         }
       }
     }
@@ -21,9 +20,26 @@ $(document).ready(() => {
         });
     }
 
+    function moveCacador(x, y) {
+      const cacador = $('#tabuleiro .personagem.cacador');
+      let posicao = cacador.attr('class').split(' ')[0].split('-');
+      posicao.shift();
+      posicao = {
+        x: parseInt(posicao[0]),
+        y: parseInt(posicao[1])
+      };
+      console.log('classes antes', cacador.attr('class'));
+      cacador.removeClass('personagem');
+      cacador.removeClass('cacador');
+      console.log('classes depois', cacador.attr('class'));
+      console.log(posicao);
+      // console.log(cacador);
+    }
+
     montaTabuleiro(10);
     preencheTabuleiro({
       cheiro: [{x: 2, y: 2}],
       cacador: [{x: 1, y: 2}, {x: 2, y: 2}],
-    })
+    });
+    // moveCacador(4, 4);
 });
